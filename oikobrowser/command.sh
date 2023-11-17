19cd9d2ff5ff6f8524f78cc6f2ee23e4919c97fb
Johannes Nicolaus Wibisana <johannes.nicolaus@oist.jp>
2023-11-17 10:09:00 +0900

    add gtf version and replace gff3 with gtf due to conversion bug

---
+++ oikobrowser/command.sh
@@ -0,0 +1,5 @@
+# commands run on 2023/11/17
+jbrowse add-track https://raw.githubusercontent.com/oist/Oikopleuradioica_genomeannotation/main/Bar2_p4/v1.1/Bar2_p4_5primeutr_v1.1.gtf -a Bar2_p4 -n Bar2_p4_5primeutr_v1.1.gtf -d "v1 with UTRs inferred from CAGE peaks"
+jbrowse add-track https://raw.githubusercontent.com/oist/Oikopleuradioica_genomeannotation/main/OKI.I69/v2.1/OKI2018_I69_5primeutr_v2.1.gtf -a OKI.I69 -n OKI2018_I69_5primeutr_v2.1.gtf -d "v2 with UTRs inferred from CAGE peaks"
+jbrowse add-track https://raw.githubusercontent.com/oist/Oikopleuradioica_genomeannotation/main/v2.1/OSKA2016_5primeutr_v2.1.gtf -a OSKA2016v1.9 -n OSKA2016_5primeutr_v2.1.gtf -d "v2 with UTRs inferred from CAGE peaks"
+jbrowse add-track https://raw.githubusercontent.com/oist/Oikopleuradioica_genomeannotation/main/Bar2_p4/v4/Bar2_p4_5primeutr_v4.0.gtf -a Bar2_p4 -n Bar2_p4_5primeutr_v4.0.gtf -d "v4 with UTRs inferred from CAGE peaks"
